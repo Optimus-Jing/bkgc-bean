b57fc0cffca6caec517ee84351d1e6f666022168
张付涛 <zftbank@qq.com>
2018-03-06 18:43:45 +0800

    测试提交权限

--- src/test/java/com/bkgc/bean/AppTest.java
+++ src/test/java/com/bkgc/bean/AppTest.java
@@ -11,7 +11,7 @@ public class AppTest
     extends TestCase
 {
     /**
-     * Create the test case
+     * Create the test case 测试提交权限
      *
      * @param testName name of the test case
      */
